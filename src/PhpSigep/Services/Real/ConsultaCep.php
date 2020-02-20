a1063b2b1e8f95d741177e1b9327d83c3f182161
Dinesh Kumar <dinesharya2890@gmail.com>
2020-02-20 18:53:43 +0530

    fix empty and isset data

--- src/PhpSigep/Services/Real/ConsultaCep.php
+++ src/PhpSigep/Services/Real/ConsultaCep.php
@@ -33,26 +33,57 @@ class ConsultaCep
              if (property_exists($r, 'return') && $r->return instanceof \stdClass) {
                 $consultaCepResposta = new ConsultaCepResposta();
 				
-				$bairro = isset(SoapClientFactory::convertEncoding($r->return->bairro)) ? SoapClientFactory::convertEncoding($r->return->bairro) : "";
-                $consultaCepResposta->setBairro($bairro);
-				$cep = isset($r->return->cep) ? $r->return->cep : "";
+				$bairro = '';
+				if (!empty($r->return->bairro) && null !== $r->return->bairro) {
+					$bairro = SoapClientFactory::convertEncoding($r->return->bairro);
+				}
+				$consultaCepResposta->setBairro($bairro);
+                
+				$cep = '';
+				if (!empty($r->return->cep) && null !== $r->return->cep) {
+					$cep = SoapClientFactory::convertEncoding($r->return->cep);
+				}
                 $consultaCepResposta->setCep($cep);
-				$cidade = isset(SoapClientFactory::convertEncoding($r->return->cidade)) ? SoapClientFactory::convertEncoding($r->return->cidade) : "";
+				
+				$cidade = '';
+				if (!empty($r->return->cidade) && null !== $r->return->cidade) {
+					$cidade = SoapClientFactory::convertEncoding($r->return->cidade);
+				}
                 $consultaCepResposta->setCidade($cidade);
-				$complemento = isset(SoapClientFactory::convertEncoding($r->return->complemento)) ? SoapClientFactory::convertEncoding($r->return->complemento) : "";
-                $consultaCepResposta->setComplemento1($complemento);
-				$complemento2 = isset(SoapClientFactory::convertEncoding($r->return->complemento2)) ? SoapClientFactory::convertEncoding($r->return->complemento2) : "";
-                $consultaCepResposta->setComplemento2($complemento2);
-				$end = isset(SoapClientFactory::convertEncoding($r->return->end)) ? SoapClientFactory::convertEncoding($r->return->end) : "";
-                $consultaCepResposta->setEndereco($end);
-				$id = isset($r->return->id) ? $r->return->id : "";
+				
+				$complemento = '';
+				if (!empty($r->return->complemento) && null !== $r->return->complemento) {
+					$complemento = SoapClientFactory::convertEncoding($r->return->complemento);
+				}
+				$consultaCepResposta->setComplemento1($complemento);
+				
+				$complemento2 = '';
+				if (!empty($r->return->complemento2) && null !== $r->return->complemento2) {
+					$complemento2 = SoapClientFactory::convertEncoding($r->return->complemento2);
+				}
+				$consultaCepResposta->setComplemento2($complemento2);
+				
+				$end = '';
+				if (!empty($r->return->end) && null !== $r->return->end) {
+					$end = SoapClientFactory::convertEncoding($r->return->end);
+				}
+				$consultaCepResposta->setEndereco($end);
+				
+				$id = '';
+				if (!empty($r->return->id) && null !== $r->return->id) {
+					$id = $r->return->id;
+				}
                 $consultaCepResposta->setId($id);
-				$uf = isset($r->return->uf) ? $r->return->uf : "";
+				
+                $uf = '';
+				if (!empty($r->return->uf) && null !== $r->return->uf) {
+					$uf = $r->return->uf;
+				}
                 $consultaCepResposta->setUf($uf);
                 $result->setResult($consultaCepResposta);
              } else {
-				 $errorCode = 0;
-				 $errorMsg = "Resposta em branco. Confirme se o CEP '$cep' realmente existe.";
+                 $errorCode = 0;
+                 $errorMsg = "Resposta em branco. Confirme se o CEP '$cep' realmente existe.";
              }
         } else {
             $errorCode = 0;

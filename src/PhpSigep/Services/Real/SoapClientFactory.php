a30b51907b9f7294d4701d20e82d69302fed3730
Webnstack Pvt. Ltd <dinesharya2890@gmail.com>
2020-02-20 16:06:45 +0530

    remove ciphers

--- src/PhpSigep/Services/Real/SoapClientFactory.php
+++ src/PhpSigep/Services/Real/SoapClientFactory.php
@@ -30,7 +30,6 @@ class SoapClientFactory
 
             $opts = array(
                 'ssl' => array(
-                    'ciphers'           =>'RC4-SHA', 
                     'verify_peer'       =>false, 
                     'verify_peer_name'  =>false
                 )
